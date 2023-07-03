f30f1b67c6258d011d2ae142a1c83d742ac412ca
mat4rte <didiandrevsc@gmail.com>
2023-07-03 19:58:19 +0100

    nav update

--- front/src/components/ui/Nav/nav.js
+++ front/src/components/ui/Nav/nav.js
@@ -7,10 +7,9 @@ function Nav() {
   return (
     <>
       <div className="navbar flex justify-left mx-10 items-center mt-6 text-main font-medium font-main justify-around flex-wrap gap-20 max-lg:gap-4 text-base max-lg:text-xs">
-        <div className="basis-1/3 shrink-0 grow">
+        <a href="/" className="basis-1/3 shrink-0 grow">
           <Logo class="navbar-height" />
-        </div>
-        {/* <div className="nav-pages basis-2/3 flex justify-end gap-x-24 flex-wrap"> */}
+        </a>
         <div className="text-center hover:underline">
           <ButtonNavBar text="Calendar" url="/calendar" />
         </div>
@@ -23,7 +22,6 @@ function Nav() {
         <div className="text-center">
           <ButtonNavBar text="Mata" url="/account" />
         </div>
-        {/* </div> */}
       </div>
     </>
   );
